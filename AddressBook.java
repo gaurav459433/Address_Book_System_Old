0449d83974d1db36345d76798eb46a98f6efec01
gaurav459433 <gaurav459433@gmail.com>
2023-03-05 01:18:08 +0530

    Added: Code For Add Contact To Address Book

---
+++ AddressBook.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.assignmentsrfp259.day09.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+
+    ContactPerson contact = new ContactPerson();
+
+    public void addContact() {
+        System.out.println("Enter your FirstName: ");
+        contact.setFirstName(sc.next());
+        System.out.println("Enter your LastName: ");
+        contact.setLastName(sc.next());
+        System.out.println("Enter your Email: ");
+        contact.setEmail(sc.next());
+        System.out.println("Enter your Address: ");
+        contact.setAddress(sc.next());
+        System.out.println("Enter your City: ");
+        contact.setCity(sc.next());
+        System.out.println("Enter your State: ");
+        contact.setState(sc.next());
+        System.out.println("Enter your Zipcode: ");
+        contact.setZip(sc.nextInt());
+        System.out.println("Enter your Mobile Number: ");
+        contact.setMobile(sc.nextLong());
+    }
+
+    public void showContacts() {
+        System.out.println(contact.toString());
+    }
+}
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,30 @@
 package com.bridgelabz.assignmentsrfp259.day09.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book Program: ");
-        ContactPerson cp = new ContactPerson();
-        System.out.println(cp.toString());
+        AddressBook addressBook = new AddressBook();
+        int option;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT ");
+            System.out.println("Enter the Operation Number");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.showContacts();
+                    break;
+                default:
+                    System.out.println("Wrong Operation Number");
+                    break;
+            }
+        } while (option < 3);
     }
 }
