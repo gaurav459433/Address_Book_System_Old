a20613573430438039bb5072bbce1b3056d57c06
gaurav459433 <gaurav459433@gmail.com>
2023-03-05 01:06:06 +0530

    Added: Code For Create Contact With All Parameters

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelabz.assignmentsrfp259.day09.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book Program: ");
+        ContactPerson cp = new ContactPerson();
+        System.out.println(cp.toString());
     }
 }
